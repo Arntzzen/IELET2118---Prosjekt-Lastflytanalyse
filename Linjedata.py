5d4baecc9ca35e73cfa421a47358638ebe5c8df2
Markus Arntzen <arntzen@dhcp-10-22-60-97.wlan.ntnu.no>
2025-11-02 14:03:38 +0100

    Fikset beregning for admittanser og gjort eksporten til excel finere.

--- Linjedata.py
+++ Linjedata.py
@@ -2,6 +2,9 @@ import numpy as np
 from prettytable import PrettyTable
 import pandas as pd
 
+
+
+##################### Linjedata ###################
 A = (0.04 + 0.43j, 8.7, 600)
 B = (0.03 + 0.33j, 11.3, 900)
 C = (0.02 + 0.32j, 11.4, 1000)
@@ -11,11 +14,12 @@ lengder = np.array([22.9, 18.8, 47.8, 12.1, 71.2, 54.4, 49.4, 105.8, 116.7])
 klasser = np.array([A, A, A, B, C, C, A, A, A], dtype=object)
 
 
+
+################### Beregninger ###################
 impedanser = {linjer[i]: lengder[i] * klasser[i][0] for i in range(len(linjer))}
 impedanser_pu = {linje: Z / 90 for linje, Z in impedanser.items()}
-
-
 kapasitanser = {linjer[i]: lengder[i] * klasser[i][1] for i in range(len(linjer))}
+admittanser_pu = {linje: 1 / Z for linje, Z in impedanser_pu.items()}
 
 
 
@@ -23,42 +27,56 @@ kapasitanser = {linjer[i]: lengder[i] * klasser[i][1] for i in range(len(linjer)
 def round_complex(z):
     return complex(round(z.real, 4), round(z.imag, 4))
 
-# Lag DataFrame og rund alt
 df_imp = pd.DataFrame({
     "Linje": list(impedanser.keys()),
     "Impedans (ohm)": [round_complex(z) for z in impedanser.values()],
     "Impedans (p.u.)": [round_complex(z) for z in impedanser_pu.values()],
+    "Admittans (p.u.)": [round_complex(y) for y in admittanser_pu.values()],
     "Kapasitans (nF)": [round(c) for c in kapasitanser.values()]
 })
-
-# Skriv til Excel
 df_imp.to_excel("linjeimpedanser.xlsx", index=False)
 print(df_imp)
 
 
 
-################### PrettyTable utskrift ###################
-linjeimpedanser = PrettyTable()
-linjeimpedanser.field_names = ["Linjeimpedanser", "Impedans (ohm)"]
+################## Excel eksport uten averunding ##################
+df_imp_full = pd.DataFrame({
+    "Linje": list(impedanser.keys()),
+    "Impedans (ohm)": list(impedanser.values()),
+    "Impedans (p.u.)": list(impedanser_pu.values()),
+    "Admittans (p.u.)": list(admittanser_pu.values()),
+    "Kapasitans (nF)": list(kapasitanser.values())
+})
 
+df_imp_full.to_excel("linjeimpedanser_full.xlsx", index=False)
+print(df_imp_full)
+
+
+
+################### PrettyTable print ###################
+linjeimpedanser = PrettyTable()
+linjeimpedanser.field_names = ["Linje", "Impedans (ohm)"]
 for key, value in impedanser.items():
     linjeimpedanser.add_row([key, round(value, 3)])
 print(linjeimpedanser)
-df_imp.to_excel('Linjeimpedanser.xlsx', index=False)
 
 
 linjekapasitanser = PrettyTable()
-linjekapasitanser.field_names = ["Linjekapasitanser", "Kapasitans (F)"]
-
+linjekapasitanser.field_names = ["Linje", "Kapasitans (F)"]
 for key, value in kapasitanser.items():
     linjekapasitanser.add_row([key, round(value, 3)])
-
 print(linjekapasitanser)
 
-linjeimpedanser_pu = PrettyTable()
-linjeimpedanser_pu.field_names = ["Linjeimpedanser i pu", "Impedans (pu)"]
 
+linjeimpedanser_pu = PrettyTable()
+linjeimpedanser_pu.field_names = ["Linje", "Impedans (pu)"]
 for key, value in impedanser_pu.items():
     linjeimpedanser_pu.add_row([key, round(value, 4)])
-
 print(linjeimpedanser_pu)
+
+
+linjeadmittanser_pu = PrettyTable()
+linjeadmittanser_pu.field_names = ["Linje", "Admittans (pu)"]
+for key, value in admittanser_pu.items():
+    linjeadmittanser_pu.add_row([key, round(value, 4)])
+print(linjeadmittanser_pu)
